b29f084c7c4a5e9d10868f2d185aa93ea38c21e5
luisjedev <luisjedev@gmail.com>
2025-07-09 19:32:07 +0200

    style: :art: Character list styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,22 @@
 import { Link } from "@tanstack/react-router";
+import Like from "@/assets/icons/like.svg?react";
+import logo from "@/assets/images/logo.png";
 import styles from "./header.module.css";
 
 export function Header() {
 	return (
-		<header className={styles.header}>
-			<Link to="/">Home</Link>
+		<header className={styles["header"]}>
+			<div className={styles["header__container"]}>
+				<Link to="/" className={styles["header__logo-link"]}>
+					<img src={logo} alt="logo" className={styles["header__logo"]} />
+				</Link>
+				<div className={styles["header__favorite-container"]}>
+					<div className={styles["header__favorite-icon-container"]}>
+						<Like className={styles["header__favorite-icon"]} />
+					</div>
+					<span className={styles["header__favorite-count"]}>2</span>
+				</div>
+			</div>
 		</header>
 	);
 }
