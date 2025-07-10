e645db2436e5616c44d33708ad429a3311b70b29
luisjedev <luisjedev@gmail.com>
2025-07-10 21:09:59 +0200

    style: :art: grid character alignment

--- src/routes/details/$id/index.tsx
+++ src/routes/details/$id/index.tsx
@@ -1,7 +1,5 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { CharacterDetailsCard } from "@/components/character-details-card/character-details-card";
 import { TransformationsList } from "@/components/transformations-list/transformations-list";
 import { characterByIdQueryOptions } from "@/services/get-character-by-id";
@@ -21,13 +19,9 @@ function RouteComponent() {
 	const { data } = useSuspenseQuery(characterByIdQueryOptions(id));
 
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<ErrorBoundary fallbackRender={() => <div>Error</div>}>
-				<main className={"details-container"}>
-					<CharacterDetailsCard character={data} />
-					<TransformationsList transformations={data.transformations} />
-				</main>
-			</ErrorBoundary>
-		</Suspense>
+		<main className={"details-container"}>
+			<CharacterDetailsCard character={data} />
+			<TransformationsList transformations={data.transformations} />
+		</main>
 	);
 }
